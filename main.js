9bd87d4e5a240dbb4b375623f010addceae70912
Ivy356 <54084140+Ivy356@users.noreply.github.com>
2019-12-05 11:05:13 -0600

    Update main.js

--- main.js
+++ main.js
@@ -4,3 +4,7 @@ function moneyButton(){
   money = money + 1;
   document.getElementById("money").innerHTML = money;
 };
+
+const canvas = document.querySelector("canvas");
+canvas.width = 500;
+canvas.height = 300;
